345c660d6ab843baa55339e526afa67f0df082ed
88147999+exeProgram@users.noreply.github.com <88147999+exeProgram@users.noreply.github.com>
2022-02-05 13:11:02 +0000

    updates

--- dist/current/JS/components.js
+++ dist/current/JS/components.js
@@ -44,10 +44,10 @@ class Header extends HTMLElement {
         <div id="devConsole" class="w-screen h-screen z-30 fixed top-0 bg-opacity-60 bg-black flex justify-center items-center">
         <div class="bg-zinc-700 rounded-lg p-4 flex items-center flex-col">
             <h1 class="text-center text-xl font-bold p-2 text-white">Dev Console</h1>
-            <h3 class="text-center text-white">Congrats! You found the Dev Console, have a reward!</h3>
+            <h3 class="text-center text-white">Congrats! You found the (Work In Progress) Dev Console. While the Dev Console has yet to developed, heres a video you can watch in the meantime</h3>
             <span class="h-2 block" style="height: 15px"></span>
             <a href="https://youtu.be/dQw4w9WgXcQ" class="navigation-button text-xl pl-8 pr-8 mt-4 mb-4 w-full text-center">
-                Reward
+                Watch Video
             </a>
             <a href="#" onclick="devConsoleClose()" class="navigation-button nav-red text-xl pl-8 pr-8 w-full text-center">
                 Close
